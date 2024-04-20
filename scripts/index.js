06eaafaf1d80b0ec14829ac428c58f236d6e1da0
Alexander Krylov <a.v.krylovv@yandex.ru>
2024-04-20 18:10:01 +0300

    refactor: функционал изменен на создание карточки, добавление обработчика для удаления карточки и функцию вывода карточкек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
+const cardsContainer = document.querySelector('.places__list');
+
 // @todo: Функция удаления карточки
 const deleteParentElement = (element) => {
   element.parentElement.remove();
 }
 
 // @todo: Функция создания карточки
-const addCard = (nameCard, linkImg, deleteParentElement) => {
+const createCard = (nameCard, linkImg, deleteParentElement) => {
   // @todo: Темплейт карточки
   const cardTemplate = document.querySelector('#card-template').content;
-  // @todo: DOM узлы
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  const cardsContainer = document.querySelector('.places__list');
-
+  // @todo: DOM узлы
   const cardImage = cardElement.querySelector('.card__image');
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   const cardTitle = cardElement.querySelector('.card__title');
 
   cardImage.src = linkImg;
-  cardImage.alt = `Реальность имеет свои пределы. Мир воображения безграничен. Представь, что тут фото - ${nameCard}`;
+  cardImage.alt = `Реальность имеет свои пределы. Мир воображения безграничен. Представьте, что тут фото - ${nameCard}`;
   cardTitle.textContent = nameCard;
 
   cardDeleteButton.addEventListener('click', (evt) => {
     deleteParentElement(evt.target);
   });
-
-  cardsContainer.append(cardElement);
+  
+  return cardElement;
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach( (objCard) => {
-  addCard(objCard.name, objCard.link, deleteParentElement);
+  cardsContainer.append(createCard(objCard.name, objCard.link, deleteParentElement));
 })
